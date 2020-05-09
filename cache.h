890f2c85cd1e4951a6cbda6877c009bcf44d636e
Zhang.Kang <Kang.Zhang@dell.com>
2020-05-09 16:02:07 +0800

    fix issue

--- cache.h
+++ cache.h
@@ -6,6 +6,6 @@ private:
 public:
     Cache(int size) : size_(size) {};
     virtual ~Cache() {};
-    virtual uint8_t* Data(int x) const;
+    virtual uint8_t* Data(int x) const = 0;
     int Size() {return size_;};
 };
